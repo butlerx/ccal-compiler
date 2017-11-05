521e6499668f90d7f2679dcd44707810adc5f33d
butlerx <butlerx@notthe.cloud>
2017-11-05 13:21:08 +0000

    add second grammer

--- test.sh
+++ test.sh
@@ -1,2 +1,17 @@
-javacc ccal.jj && javac *.java &&
-for script in test/*.ccal; do echo $script; java Ccal $script || exit 1; done
+#! /usr/bin/env sh
+
+set -e
+
+noGram () {
+  echo "Grammer doesn't exist"
+  exit 1
+}
+
+GRAM=$1
+cd grammers/"$GRAM" 2> /dev/null || noGram
+javacc ccal.jj
+javac ./*.java
+for script in test/*.ccal; do
+  echo "$script"
+  java Ccal "$script" || exit 1
+done
