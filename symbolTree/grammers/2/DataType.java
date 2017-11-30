1fcd0db9b935e94c5396123fc57396066d635472
butlerx <butlerx@notthe.cloud>
2017-11-30 12:08:02 +0000

    add java files

---
+++ symbolTree/grammers/2/DataType.java
@@ -0,0 +1,34 @@
+// Name:DataType.java
+// Author: David Sinclair      Date: 29 Aug 2012
+//
+
+public enum DataType
+{
+    Program,
+    Declaration,
+    TypeUnknown,
+    TypeInteger,
+    TypeBoolean,
+    ConstDeclaration,
+    VarDeclaration,
+    Function,
+    ParamList,
+    MainProg,
+    Int,
+    Bool,
+    Void,
+    Equal,
+    NotEqual,
+    LessThan,
+    GreaterThan,
+    LTE,
+    GTE,
+    Assign,
+    Plus,
+    Minus,
+    Div,
+    Mult,
+    Condition,
+    ArgList,
+    Num
+}
